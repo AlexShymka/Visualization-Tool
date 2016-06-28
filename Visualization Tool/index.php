989c717eb63b6cf76de265023fe7cc377a9fbb6a
AlexShymka <androidaccountalex@gmail.com>
2016-06-28 14:36:45 -0600

    Who Knows

---
+++ Visualization Tool/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>PHP</title>
+</head>
+<body>
+	<p>
+      <?php
+						echo "The Foundation for the visualization tool!";
+						?>
+		
+	
+	
+	<form action="welcome.php" method="post">
+		Name: <input type="text" name="name"><br> E-mail: <input type="text"
+			name="email"><br> <input type="submit" value="Go">
+	</form>
+	</p>
+</body>
+</html>
---
+++ Visualization Tool/welcome.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+	<?php
+	
+$name = $_POST ["name"];
+	$email = $_POST ["email"];
+	?>
+		Welcome <?php echo $name; ?><br>
+		Your email address is: <?php echo $email; ?>
+		
+	</body>
+</html>
